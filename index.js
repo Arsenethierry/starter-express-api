ab2368d5df5f1a08ad41e3eb1bbc23b157e6417d
Arsene <komezarsene15@gmail.com>
2023-01-25 12:34:28 +0530

    prod

--- index.js
+++ index.js
@@ -1,6 +1,13 @@
 const express = require('express')
 const products = require('./products');
+const cors = require('cors');
+const mongoose = require('mongoose');
+
 const app = express()
+require('dotenv').config()
+app.use(cors());
+app.use(express.json());
+
 app.all('/', (req, res) => {
     console.log("Just got a request!")
     res.send('Yo!')
@@ -9,4 +16,12 @@ app.all('/', (req, res) => {
 app.get('/products', (req, res) => {
     res.send(products);
 })
+const dburl = process.env.DB_URL
+
 app.listen(process.env.PORT || 3000)
+
+mongoose.connect(dburl, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+}).then(() => console.log("connected to mongoDb"))
+.catch((error) => console.log(`error ${error}`))
