c678a76410c50e4421b146149df70fd548ba205d
Arsene <komezarsene15@gmail.com>
2023-01-25 11:58:56 +0530

    products

--- index.js
+++ index.js
@@ -1,7 +1,12 @@
 const express = require('express')
+const products = require('./products');
 const app = express()
 app.all('/', (req, res) => {
     console.log("Just got a request!")
     res.send('Yo!')
+});
+
+app.get('/products', (req, res) => {
+    res.send(products);
 })
 app.listen(process.env.PORT || 3000)
